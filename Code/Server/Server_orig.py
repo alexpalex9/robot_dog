ab727386b89821071d4dec23eac8903a1c4a9cae
alexpalex9 <alexpanda9@hotmail.com>
2020-11-16 14:04:34 +0100

    web server instead

--- Code/Server/Server_orig.py
+++ Code/Server/Server_orig.py
@@ -16,84 +16,6 @@ from ADS7830 import *
 from Ultrasonic import *
 from Command import COMMAND as cmd
 
-import logging
-import socketserver
-from threading import Condition
-from http import server
-
-PAGE="""\
-<html>
-<head>
-<title>Raspberry Pi - Surveillance Camera</title>
-</head>
-<body>
-<center><h1>Raspberry Pi - Surveillance Camera</h1></center>
-<center><img src="stream.mjpg" width="640" height="480"></center>
-</body>
-</html>
-"""
-
-class StreamingOutput(object):
-    def __init__(self):
-        self.frame = None
-        self.buffer = io.BytesIO()
-        self.condition = Condition()
-
-    def write(self, buf):
-        if buf.startswith(b'\xff\xd8'):
-            # New frame, copy the existing buffer's content and notify all
-            # clients it's available
-            self.buffer.truncate()
-            with self.condition:
-                self.frame = self.buffer.getvalue()
-                self.condition.notify_all()
-            self.buffer.seek(0)
-        return self.buffer.write(buf)
-
-class StreamingHandler(server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            self.send_response(301)
-            self.send_header('Location', '/index.html')
-            self.end_headers()
-        elif self.path == '/index.html':
-            content = PAGE.encode('utf-8')
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Length', len(content))
-            self.end_headers()
-            self.wfile.write(content)
-        elif self.path == '/stream.mjpg':
-            self.send_response(200)
-            self.send_header('Age', 0)
-            self.send_header('Cache-Control', 'no-cache, private')
-            self.send_header('Pragma', 'no-cache')
-            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-            self.end_headers()
-            try:
-                while True:
-                    with self.output.condition:
-                        self.output.condition.wait()
-                        frame = self.output.frame
-                    self.wfile.write(b'--FRAME\r\n')
-                    self.send_header('Content-Type', 'image/jpeg')
-                    self.send_header('Content-Length', len(frame))
-                    self.end_headers()
-                    self.wfile.write(frame)
-                    self.wfile.write(b'\r\n')
-            except Exception as e:
-                logging.warning(
-                    'Removed streaming client %s: %s',
-                    self.client_address, str(e))
-        else:
-            self.send_error(404)
-            self.end_headers()
-
-class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
-    allow_reuse_address = True
-    daemon_threads = True
-    
-    
 class Server:
     def __init__(self):
         
@@ -116,23 +38,10 @@ class Server:
         #ip adress
         HOST=self.get_interface_ip()
         #Port 8000 for video transmission
-        #self.server_socket = socket.socket()
-        #self.server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEPORT,1)
-        #self.server_socket.bind((HOST, 8001))              
-        #self.server_socket.listen(1)
-        
-        
-        with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
-            self.output = StreamingOutput()
-            #Uncomment the next line to change your Pi's Camera rotation (in degrees)
-            #camera.rotation = 90
-            camera.start_recording(self.output, format='mjpeg')
-            try:
-                address = ('', 8000)
-                server = StreamingServer(address, StreamingHandler)
-                server.serve_forever()
-            finally:
-                camera.stop_recording()
+        self.server_socket = socket.socket()
+        self.server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEPORT,1)
+        self.server_socket.bind((HOST, 8001))              
+        self.server_socket.listen(1)
         
         #Port 5000 is used for instruction sending and receiving
         self.server_socket1 = socket.socket()
