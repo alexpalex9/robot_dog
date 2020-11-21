dcd7ffc3d3ce87a4168669fedb8835d2652298eb
alexpalex <alexpanda9@hotmail.com>
2020-11-21 14:47:04 +0100

    clean rect

--- Code/Server/view/blaze_pred.js
+++ Code/Server/view/blaze_pred.js
@@ -21,23 +21,23 @@
           if (predictions.length > 0)
           {
            console.log("prediction found",predictions);
-           for (let i = 0; i < predictions.length; i++) {
-           const start = predictions[i].topLeft;
-           const end = predictions[i].bottomRight;
-           var probability = predictions[i].probability;
-           const size = [end[0] - start[0], end[1] - start[1]];
-           // Render a rectangle over each detected face.
-		   context.clearRect(0, 0, canvas.width, canvas.height);
-           context.beginPath();
-           context.strokeStyle="green";
-           context.lineWidth = "4";
-           context.rect(start[0], start[1],size[0], size[1]);
-           context.stroke();
-           var prob = (probability[0]*100).toPrecision(5).toString();
-           var text = prob+"%";
-           context.fillStyle = "red";
-           context.font = "13pt sans-serif";
-           context.fillText(text,start[0]+5,start[1]+20);
+			 for (let i = 0; i < predictions.length; i++) {
+			   const start = predictions[i].topLeft;
+			   const end = predictions[i].bottomRight;
+			   var probability = predictions[i].probability;
+			   const size = [end[0] - start[0], end[1] - start[1]];
+			   // Render a rectangle over each detected face.
+			   // context.clearRect(0, 0, canvas.width, canvas.height);
+			   context.beginPath();
+			   context.strokeStyle="green";
+			   context.lineWidth = "4";
+			   context.rect(start[0], start[1],size[0], size[1]);
+			   context.stroke();
+			   var prob = (probability[0]*100).toPrecision(5).toString();
+			   var text = prob+"%";
+			   context.fillStyle = "red";
+			   context.font = "13pt sans-serif";
+			   context.fillText(text,start[0]+5,start[1]+20);
             }
            }
         setTimeout(draw,2000,video,canvas,width,height);
