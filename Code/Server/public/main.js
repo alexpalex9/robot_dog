34b2f1f6d0e19c290c997e765dfec1f4a567d970
alexpalex <alexpanda9@hotmail.com>
2021-01-17 15:03:19 +0100

    test first command

--- Code/Server/public/main.js
+++ Code/Server/public/main.js
@@ -3,6 +3,29 @@ var init = function(){
 		face_detection :{
 			$canvas : $('#face_overlay')
 		}
+		COMMAND : {
+		    CMD_MOVE_STOP : "CMD_MOVE_STOP",
+			CMD_MOVE_FORWARD : "CMD_MOVE_FORWARD" ,
+			CMD_MOVE_BACKWARD : "CMD_MOVE_BACKWARD",
+			CMD_MOVE_LEFT : "CMD_MOVE_LEFT",
+			CMD_MOVE_RIGHT : "CMD_MOVE_RIGHT",
+			CMD_TURN_LEFT : "CMD_TURN_LEFT",
+			CMD_TURN_RIGHT : "CMD_TURN_RIGHT",
+			CMD_BUZZER : "CMD_BUZZER",
+			CMD_LED_MOD : "CMD_LED_MOD",
+			CMD_LED : "CMD_LED",
+			CMD_BALANCE : "CMD_BALANCE",
+			CMD_SONIC : "CMD_SONIC",
+			CMD_HEIGHT : "CMD_HEIGHT",
+			CMD_HORIZON : "CMD_HORIZON",
+			CMD_HEAD : "CMD_HEAD",
+			CMD_CALIBRATION : "CMD_CALIBRATION",
+			CMD_POWER : "CMD_POWER",
+			CMD_ATTITUDE : "CMD_ATTITUDE",
+			CMD_RELAX : "CMD_RELAX",
+			CMD_WORKING_TIME : "CMD_WORKING_TIME"	
+			
+		}
 	};
 	var url = "http://" + document.domain + ":" + location.port;
 	_this.socket = io.connect(url + "/robot");
@@ -11,7 +34,7 @@ var init = function(){
     
 	$('html').on('click','.cmd',function(){
 			console.log("click emit")
-			_this.socket.emit('cmd',  $(this).data('v')) // {'episodes': $('#episodes').val() , });
+			_this.socket.emit('cmd',  $(this).data('cmd')) // {'episodes': $('#episodes').val() , });
 		// console.log("url =",'cmd?cmd=' + $(this).data('cmd') + '&value=' + $(this).data('v'))
 		// $.ajax({
 			// url : '/cmd&cmd=' + $(this).data('cmd') + '&value=' + $(this).data('v'),
