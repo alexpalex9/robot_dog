dbf2f80252990ef25d76427d9971274075643c8b
alexpalex <alexpanda9@hotmail.com>
2021-02-07 16:42:56 +0100

    rewaerd only gyro

--- Code/Server/public/a2c.js
+++ Code/Server/public/a2c.js
@@ -616,7 +616,7 @@ function actor_critic() {
 								rewardSonic = 0
 							}
 							// reward = 1/2 * rewardSonic / 10 + 1/6 - Math.abs(gyro.x) / 100 + 1/6 - Math.abs(gyro.y) / 100 + 1/6 - Math.abs(gyro.z) / 100
-							reward =  1/3 *  1 (- Math.abs(gyro.x) / 100 ) + 1/3 * (1 - Math.abs(gyro.y) / 100 ) + 1/3 * (1- Math.abs(gyro.z) / 100)
+							reward =  1/3 *  (1- Math.abs(gyro.x) / 100 ) + 1/3 * (1 - Math.abs(gyro.y) / 100 ) + 1/3 * (1- Math.abs(gyro.z) / 100)
 							console.log("REWARD",reward)
 							// reward = 0.5
 							
